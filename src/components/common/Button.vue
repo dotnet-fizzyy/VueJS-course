eca2519280ca6f9b347187cd88535c06d5d8f978
Dmitry Yaniuk <ezzyfizzy27@gmail.com>
2022-04-10 15:13:06 +0300

    VueComponents: Implement Button component, add it to Storybook, remove default Storybook example, add SASS preprocessor to simplify layout, add styles with app theme

---
+++ src/components/common/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button v-bind:class="style" v-on:click="onClick"><slot /></button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export enum FontStyle {
+    Normal = 'normal',
+    Bold = 'bold',
+    Italic = 'italic',
+}
+
+export default Vue.extend({
+    name: 'Button',
+    props: {
+        fontStyle: {
+            type: String,
+            default: FontStyle.Normal,
+        },
+        onClick: {
+            type: Function,
+            required: true,
+        },
+    },
+    computed: {
+        style(): string[] {
+            return ['primary-button', this.fontStyle];
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/buttons';
+@import '../../styles/fonts';
+</style>
---
+++ src/stories/common/Button.stories.js
@@ -0,0 +1,14 @@
+import Button, { FontStyle } from '../../components/common/Button';
+import { action } from '@storybook/addon-actions';
+
+export default {
+    title: 'Button',
+};
+
+export const Default = () => ({
+    components: { Button },
+    template:
+        '<div style="width: 100px; height: 30px"><Button :onClick="onClick" :font-style="fontWeight">{{ title }}</Button></div>',
+    data: () => ({ title: 'Hello world!', fontWeight: FontStyle.Normal }),
+    methods: { onClick: action('clicked') },
+});
