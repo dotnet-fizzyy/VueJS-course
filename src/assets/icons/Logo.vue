043b6f290b5afe215d2b1e9e775a720ab30f6e99
Dmitry Yaniuk <ezzyfizzy27@gmail.com>
2022-04-14 14:55:18 +0300

    VueComponentsMainPage: Add application logo component in assets, add log to footer and header

---
+++ src/assets/icons/Logo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <span :class="$style['first-logo-part']">Netflix</span>
+        <span :class="$style['second-logo-part']">Roulette</span>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'Logo',
+});
+</script>
+
+<style lang="scss" module scoped>
+@import '../../styles/fonts';
+
+.common-logo {
+    font-family: $roboto;
+    color: $dark-pink;
+    font-size: 20px;
+    text-transform: lowercase;
+}
+
+.first-logo-part {
+    @extend .common-logo;
+    @extend .bold;
+}
+
+.second-logo-part {
+    @extend .common-logo;
+}
+</style>
--- src/components/views/StartView.vue
+++ src/components/views/StartView.vue
@@ -3,6 +3,10 @@
         <div :class="$style.header">
             <div :class="$style['header-background']">
                 <div :class="$style['search-container']">
+                    <div :class="$style['logo-container']">
+                        <logo />
+                    </div>
+
                     <search-panel />
                 </div>
             </div>
@@ -12,20 +16,24 @@
             <div :class="$style['sort-panel-container']">
                 <sort-panel :available-items-count="7" />
             </div>
+            <div></div>
         </div>
 
-        <div :class="$style.footer"></div>
+        <div :class="$style.footer">
+            <logo />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
+import Logo from '../../assets/icons/Logo.vue';
 import SearchPanel from '@/components/search/SearchPanel.vue';
 import SortPanel from '@/components/listing/SortPanel.vue';
 import Vue from 'vue';
 
 export default Vue.extend({
     name: 'StartView',
-    components: { SearchPanel, SortPanel },
+    components: { SearchPanel, SortPanel, Logo },
 });
 </script>
 
@@ -49,8 +57,15 @@ export default Vue.extend({
 
 .header-background {
     height: 100%;
-    background-color: getRgbaBlack(0.7);
+    background-color: getRgbaBlack(0.8);
     backdrop-filter: blur(3px);
+    position: relative;
+}
+
+.logo-container {
+    position: absolute;
+    left: 60px;
+    top: 20px;
 }
 
 .search-container {
@@ -72,9 +87,15 @@ export default Vue.extend({
     display: flex;
     align-items: center;
     flex-direction: row;
-    padding: 0 100px;
+    padding: 0 120px;
 }
 
 .footer {
+    height: 70px;
+    background-color: $light-grey;
+    margin-top: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
