7776976e87ac006695eb5ffd96e19f61105d4d9e
Dmitry Yaniuk <ezzyfizzy27@gmail.com>
2022-04-15 15:51:02 +0300

    VueComponentsMainPage: Split StartPage into several blocks (Header, Body, Footer), update StartPage data and handlings

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,168 +1,47 @@
 <template>
     <div :class="$style.root">
-        <div :class="$style.header">
-            <div :class="$style['header-background']">
-                <div :class="$style['search-container']">
-                    <div :class="$style['logo-container']">
-                        <logo-icon />
-                    </div>
+        <start-page-header :selected-film-id="selectedFilmId" :back-to-search="backToSearch" />
 
-                    <template v-if="!!selectedFilm">
-                        <div :class="$style['search-icon-container']" @click="onBackToSearch">
-                            <search-icon />
-                        </div>
+        <start-page-body :select-film="selectFilm" />
 
-                        <film-full-description
-                            :id="selectedFilm.id"
-                            :name="selectedFilm.name"
-                            :short-description="selectedFilm.shortDescription"
-                            :full-description="selectedFilm.fullDescription"
-                            :release-year="selectedFilm.releaseYear"
-                            :poster-url="selectedFilm.posterUrl"
-                            :poster-alt="selectedFilm.posterAlt"
-                            :rating="selectedFilm.rating"
-                            :runtime="selectedFilm.runtime"
-                        />
-                    </template>
-                    <template v-else>
-                        <search-panel />
-                    </template>
-                </div>
-            </div>
-        </div>
-
-        <div :class="$style.body">
-            <div :class="$style['sort-panel-container']">
-                <sort-panel :available-items-count="7" />
-            </div>
-
-            <div :class="$style['films-display']">
-                <div v-for="film in films" :key="film.id" :class="$style['film-container']">
-                    <film-short-description
-                        :id="film.id"
-                        :name="film.name"
-                        :short-description="film.shortDescription"
-                        :release-year="film.releaseYear"
-                        :poster-url="film.posterUrl"
-                        :poster-alt="film.posterAlt"
-                        @select="selectFilm"
-                    />
-                </div>
-            </div>
-        </div>
-
-        <div :class="$style.footer">
-            <logo-icon />
-        </div>
+        <start-page-footer />
     </div>
 </template>
 
 <script lang="ts">
-import FilmFullDescription from '@/components/films/FilmFullDescription.vue';
-import FilmShortDescription from '@/components/films/FilmPreview.vue';
-import LogoIcon from '@/components/icons/LogoIcon.vue';
-import SearchIcon from '@/components/icons/SearchIcon.vue';
-import SearchPanel from '@/components/search/SearchPanel.vue';
-import SortPanel from '@/components/listing/SortPanel.vue';
+import StartPageBody from '@/pages/start-page/StartPageBody.vue';
+import StartPageFooter from '@/pages/start-page/StartPageFooter.vue';
+import StartPageHeader from '@/pages/start-page/StartPageHeader.vue';
 import Vue from 'vue';
-import { fullFilmsDescription, shortFilmsDescription } from '@/mocks/mockFilms';
 
 export default Vue.extend({
     name: 'StartPage',
-    components: { LogoIcon, SearchIcon, FilmFullDescription, FilmShortDescription, SearchPanel, SortPanel },
+    components: {
+        StartPageFooter,
+        StartPageHeader,
+        StartPageBody,
+    },
     data() {
         return {
-            films: shortFilmsDescription,
-            selectedFilm: null,
+            selectedFilmId: '',
         };
     },
     methods: {
         selectFilm(id: string): void {
-            this.selectedFilm = fullFilmsDescription.find(x => x.id === id);
+            this.selectedFilmId = id;
         },
-        onBackToSearch(): void {
-            this.selectedFilm = null;
+        backToSearch(): void {
+            this.selectedFilmId = '';
         },
     },
 });
 </script>
 
 <style lang="scss" module scoped>
-@import '../styles/fonts';
-
 .root {
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
-}
-
-.header {
-    background-image: url('https://res.cloudinary.com/djlynoeio/image/upload/v1649880735/vue/film-posters.jpg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-
-.header-background {
-    height: 100%;
-    background-color: getRgbaBlack(0.8);
-    backdrop-filter: blur(3px);
-    position: relative;
-}
-
-.logo-container {
-    position: absolute;
-    left: 60px;
-    top: 20px;
-}
-
-.search-container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    box-sizing: border-box;
-    padding: 100px 120px;
-}
-
-.search-icon-container {
-    position: absolute;
-    right: 60px;
-    top: 20px;
-    cursor: pointer;
-}
-
-.body {
-}
-
-.films-display {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    padding: 40px;
-}
-
-.film-container {
-    padding: 25px;
-}
-
-.sort-panel-container {
-    height: 70px;
-    background-color: $light-grey;
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    padding: 0 120px;
-}
-
-.footer {
-    height: 70px;
-    background-color: $light-grey;
-    margin-top: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 }
 </style>
---
+++ src/pages/start-page/StartPageHeader.vue
@@ -0,0 +1,87 @@
+<template>
+    <div :class="$style.root">
+        <div :class="$style.background">
+            <div :class="$style['logo-icon-container']">
+                <logo-icon />
+            </div>
+
+            <template v-if="selectedFilmId && selectedFilm">
+                <div :class="$style['search-icon-container']" @click="backToSearch">
+                    <search-icon />
+                </div>
+
+                <film-full-description v-bind="selectedFilm" />
+            </template>
+
+            <template v-else>
+                <search-panel />
+            </template>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import FilmFullDescription from '@/components/films/FilmFullDescription.vue';
+import LogoIcon from '@/components/icons/LogoIcon.vue';
+import SearchIcon from '@/components/icons/SearchIcon.vue';
+import SearchPanel from '@/components/search/SearchPanel.vue';
+import Vue from 'vue';
+import { fullFilmsDescription } from '@/mocks/mockFilms';
+
+export default Vue.extend({
+    name: 'StartPageHeader',
+    components: { SearchIcon, LogoIcon, SearchPanel, FilmFullDescription },
+    props: {
+        selectedFilmId: {
+            type: String,
+            required: true,
+        },
+        backToSearch: {
+            type: Function,
+            required: true,
+        },
+    },
+    computed: {
+        selectedFilm() {
+            return fullFilmsDescription.find(x => x.id === this.selectedFilmId);
+        },
+    },
+});
+</script>
+
+<style lang="scss" module scoped>
+@import '../../styles/fonts';
+
+.root {
+    background-image: url('https://res.cloudinary.com/djlynoeio/image/upload/v1649880735/vue/film-posters.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.background {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: getRgbaBlack(0.8);
+    backdrop-filter: blur(3px);
+    box-sizing: border-box;
+    padding: 100px 120px;
+}
+
+.logo-icon-container {
+    position: absolute;
+    left: 60px;
+    top: 20px;
+}
+
+.search-icon-container {
+    position: absolute;
+    right: 60px;
+    top: 20px;
+    cursor: pointer;
+}
+</style>
